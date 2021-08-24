6e0757e01535902a38d0d903733b3a2a7c54e854
Clement Balestrat <clement.balestrat@gmail.com>
2021-08-24 11:36:55 +1000

    wallet updated

--- queries/constants.ts
+++ queries/constants.ts
@@ -1,28 +1,23 @@
 //todo: Add correct details per wallet
 export const MEMBERS = [
   {
-    address: "0x099DbFa498d0b7Fbdf8bC6eD9FaF868454022EC2",
-    logoUrl: "/img/grants/alexander.png",
+    address: "0x1a207bEefC754735871CEEb4C506686F044B1c41",
     displayName: "ΔLΞXΔNDΞR",
   },
   {
-    address: "0xbF49B454818783D12Bf4f3375ff17C59015e66Cb",
-    logoUrl: "/img/grants/andy.png",
-    displayName: "andy",
+    address: "0x4f370B4d03D2b46CcE26F1aEFE142708E03D7FFE",
+    displayName: "CryptoToit",
   },
   {
     address: "0xF5a701D13Be654a534D14DE8Cd6a328293712405",
-    logoUrl: "/img/grants/david.png",
-    displayName: "david",
+    displayName: "DG",
   },
   {
-    address: "0x4f370B4d03D2b46CcE26F1aEFE142708E03D7FFE",
-    logoUrl: "/img/grants/cryptotoit.png",
-    displayName: "cryptotoit",
+    address: "0x099DbFa498d0b7Fbdf8bC6eD9FaF868454022EC2",
+    displayName: "clΞm",
   },
   {
-    address: "0x1a207bEefC754735871CEEb4C506686F044B1c41",
-    logoUrl: "/img/grants/member-avatar.png",
+    address: "0xbF49B454818783D12Bf4f3375ff17C59015e66Cb",
     displayName: "Rubber^Duck",
   },
 ];
