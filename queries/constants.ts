52d99107dabd817b8c718c4c82931b807dda324c
acyberduck <110026703+acyberduck@users.noreply.github.com>
2022-07-26 12:58:37 +0200

    members constants updated

--- queries/constants.ts
+++ queries/constants.ts
@@ -17,31 +17,14 @@ export const MEMBERS = [
     address: "0xbf49b454818783d12bf4f3375ff17c59015e66cb s",
   },
   {
-    displayName: "joey#1760",
-    ens: "jocke.eth",
-    address: "0x86691c1dd5d567f8e73a46feb0ddd2b42e404b6b",
-  },
-  {
-    displayName: "beachmom__/#3781",
-    ens: undefined,
-    address: "0x5753f05A0C757F38f8365c22B8f9Bd6DA763D1F5",
-  },
-  {
     address: "0x1a207bEefC754735871CEEb4C506686F044B1c41",
+    ens: "ethmemes.eth",
     displayName: "ΔLΞXΔNDΞR",
   },
   {
-    address: "0xF5a701D13Be654a534D14DE8Cd6a328293712405",
-    displayName: "DG",
-  },
-  {
-    address: "0x099DbFa498d0b7Fbdf8bC6eD9FaF868454022EC2",
-    displayName: "clΞm",
-    ens: "clem.eth",
-  },
-  {
-    address: "0xbF49B454818783D12Bf4f3375ff17C59015e66Cb",
-    displayName: "Rubber^Duck",
+    address: "0x8be60fe9F7C8d940D8DA9d5dDD0D8E0c15A4288B",
+    ens: "jvk.eth",
+    displayName: "JVK",
   },
 ];
 export const MEMBER_ADDRESSES_BY_DISPLAY_NAME = MEMBERS.reduce(
