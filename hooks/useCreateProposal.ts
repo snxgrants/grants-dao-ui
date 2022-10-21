1547fbad90385c6b306c0dc8cf48a649dd5aed14
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-10-21 11:55:08 -0400

    Add EIP 712

---
+++ hooks/useCreateProposal.ts
@@ -0,0 +1,39 @@
+import { useMutation } from "react-query";
+import { SnapshotClient, SNAPSHOT_ENS } from "../constants/snapshot";
+import Connector from "../containers/Connector";
+
+const PROPOSAL_CHOICES = ["Yes", "No"];
+
+const sanitiseTimestamp = (timestamp: number) => {
+  return Math.round(timestamp / 1e3);
+};
+
+const PROPOSAL_PERIOD = 7 * 24 * 3600 * 1000;
+
+const useCreateProposal = () => {
+  const { walletAddress, provider } = Connector.useContainer();
+
+  return useMutation(async (payload: any) => {
+    const { title, body, block } = payload;
+    const proposalStartDate = sanitiseTimestamp(new Date().getTime());
+    const proposalEndDate =
+      proposalStartDate + sanitiseTimestamp(PROPOSAL_PERIOD);
+
+    if (!!walletAddress && !!provider) {
+      await SnapshotClient.proposal(provider as any, walletAddress, {
+        title,
+        space: SNAPSHOT_ENS,
+        choices: PROPOSAL_CHOICES,
+        body,
+        start: proposalStartDate,
+        end: proposalEndDate,
+        snapshot: block,
+        plugins: JSON.stringify({}),
+        type: "single-choice",
+        discussion: "",
+      });
+    }
+  });
+};
+
+export default useCreateProposal;
