c46ba5a8a3548a43fc73dc1e1fef1e8c4af05d8c
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-10-21 12:04:57 -0400

    Force web3Provider

--- hooks/useCreateProposal.ts
+++ hooks/useCreateProposal.ts
@@ -1,3 +1,4 @@
+import { ethers } from "ethers";
 import { useMutation } from "react-query";
 import { SnapshotClient, SNAPSHOT_ENS } from "../constants/snapshot";
 import Connector from "../containers/Connector";
@@ -11,16 +12,19 @@ const sanitiseTimestamp = (timestamp: number) => {
 const PROPOSAL_PERIOD = 7 * 24 * 3600 * 1000;
 
 const useCreateProposal = () => {
-  const { walletAddress, provider } = Connector.useContainer();
+  const { walletAddress } = Connector.useContainer();
 
   return useMutation(async (payload: any) => {
     const { title, body, block } = payload;
+
     const proposalStartDate = sanitiseTimestamp(new Date().getTime());
     const proposalEndDate =
       proposalStartDate + sanitiseTimestamp(PROPOSAL_PERIOD);
+    // @ts-ignore
+    const web3 = new ethers.providers.Web3Provider(window.ethereum as any);
 
-    if (!!walletAddress && !!provider) {
-      await SnapshotClient.proposal(provider as any, walletAddress, {
+    if (!!walletAddress) {
+      await SnapshotClient.proposal(web3, walletAddress, {
         title,
         space: SNAPSHOT_ENS,
         choices: PROPOSAL_CHOICES,
