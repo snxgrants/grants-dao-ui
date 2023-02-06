0274f6a02b1d1b9f29180d0a143cb9f7c304c715
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-02-06 05:24:13 -0500

    Fix block number issue

--- hooks/useCreateProposal.ts
+++ hooks/useCreateProposal.ts
@@ -12,14 +12,17 @@ const PROPOSAL_PERIOD = 7 * 24 * 3600 * 1000;
 
 const useCreateProposal = () => {
   return useMutation(async (payload: any) => {
-    const { title, body, block } = payload;
+    const { title, body } = payload;
 
     const proposalStartDate = sanitiseTimestamp(new Date().getTime());
     const proposalEndDate =
       proposalStartDate + sanitiseTimestamp(PROPOSAL_PERIOD);
     // @ts-ignore
     const web3 = new ethers.providers.Web3Provider(window.ethereum);
-    const [account] = await web3.listAccounts();
+    const [block, [account]] = await Promise.all([
+      web3.getBlockNumber(),
+      web3.listAccounts(),
+    ]);
 
     if (!!account) {
       await SnapshotClient.proposal(web3, account, {
